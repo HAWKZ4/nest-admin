5c89ea0d9c9d61f52f46e8bc3db1cd3205f75c04
M-A <108879264+HAWKZ4@users.noreply.github.com>
2025-02-26 18:24:27 +0300

    feat: Add bcrypt package to securly hash passwords when a new user registers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,28 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ConfigService } from '@nestjs/config';
+import * as bcrypt from 'bcrypt';
 
 @Controller('')
 export class AuthController {
-  constructor(private readonly authSerAuthService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Post('register')
   async register(@Body() body) {
-    return this.authSerAuthService.create(body);
+    const { first_name, last_name, email, password } = body;
+    const salt = parseInt(
+      this.configService.get<string>('BCRYPT_SALT') ?? '10',
+      10,
+    );
+    const hashedPassword = await bcrypt.hash(password, salt);
+    return this.authService.create({
+      first_name,
+      last_name,
+      email,
+      password: hashedPassword,
+    });
   }
 }
