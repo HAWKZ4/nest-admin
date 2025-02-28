920cdb4212473898520e2e3f7aec1b15bdb10da7
M-A <108879264+HAWKZ4@users.noreply.github.com>
2025-02-28 12:53:51 +0300

    feat: Add user profile retrieval with JWT authentication

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './model/user.entity';
+import { Request } from 'express';
 
-@Controller('users')
+@Controller('')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get()
+  @Get('users')
   async all(): Promise<User[]> {
     return this.userService.all();
+  }
+
+  @Get('user')
+  async getUser(@Req() request: Request): Promise<User | null> {
+    const cookie = request.cookies['jwt'];
+
+    const data = await this.userService.findOne({ id: cookie['id'] });
+
+    return data
   }
 }
