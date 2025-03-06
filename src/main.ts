0889e6101975e9c69b8098c7563a0ff25b671e12
M-A <108879264+HAWKZ4@users.noreply.github.com>
2025-03-06 10:20:20 +0300

    chore: Enable global data transformation with ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(backendPort);
 }
 bootstrap();
