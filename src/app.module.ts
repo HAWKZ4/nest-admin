8c6fa5a127ea71a91b8155bdc62ddc040799e248
M-A <108879264+HAWKZ4@users.noreply.github.com>
2025-03-22 01:13:42 +0300

    refactor: Move database config into new module 'database.module' for sepration of concern

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,7 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { JwtMiddleware } from './auth/jwt.middleware';
 import { RoleModule } from './role/role.module';
@@ -12,30 +11,18 @@ import { ProductModule } from './product/product.module';
 import { OrderModule } from './order/order.module';
 import { APP_GUARD } from '@nestjs/core';
 import { PermissionGuard } from './permission/permission.guard';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     UserModule,
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USER'),
-        password: configService.get<string>('DB_PASS'),
-        database: configService.get<string>('DB_NAME'),
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
-    }),
     AuthModule,
     RoleModule,
     PermissionModule,
     ProductModule,
     OrderModule,
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [
